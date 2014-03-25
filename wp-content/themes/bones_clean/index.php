a019ce3650f6e5a71e24c69c6887b98f9fd31c53
deadtype <yagnyuk@me.com>
2014-03-24 17:53:12 -0700

    Updates to styles

--- wp-content/themes/bones_clean/index.php
+++ wp-content/themes/bones_clean/index.php
@@ -103,13 +103,9 @@
 	<footer class="article-footer">
 			<div class='content_wrap'>
 
-				<p class="byline vcard"><?php
-					printf( __( 'Published <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span><!-- <span class="amp">&</span> filed under %4$s.-->', 'bonestheme' ), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), bones_get_the_author_posts_link(), get_the_category_list(', '));
-				?></p>
 			</div>
 	</footer>
 
-	<?php // comments_template(); // uncomment if you want to use them ?>
 
 </article>
 
