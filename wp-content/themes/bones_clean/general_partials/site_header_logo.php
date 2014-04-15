3f4ac26fa499103796251d53057f39d862b91398
deadtype <yagnyuk@me.com>
2014-04-15 00:07:53 -0700

    Pre search site updates

--- wp-content/themes/bones_clean/general_partials/site_header_logo.php
+++ wp-content/themes/bones_clean/general_partials/site_header_logo.php
@@ -1,3 +1,3 @@
-		<div class='logo'>
-			<a href="<?php echo get_settings('home'); ?>"><img src='<?php echo get_template_directory_uri(); ?>/library/images/new_dist_logo_knockout.png'></a>
+		<div class='logo_word'>
+			<a href="<?php echo get_settings('home'); ?>"><img src='<?php echo get_template_directory_uri(); ?>/library/images/logo.png'></a>
 		</div>
