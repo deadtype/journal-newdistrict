012a79f804ea3e762cab11914a5b431c86765957
deadtype <yagnyuk@me.com>
2014-04-15 00:11:04 -0700

    Social Link Update

--- wp-content/themes/bones_clean/general_partials/social_links.php
+++ wp-content/themes/bones_clean/general_partials/social_links.php
@@ -1,6 +1,6 @@
 <div class='content_wrap'>
 	<ul class='navigation'>
-		<span class="direction">Share On:</span>
+		<span class="direction">Share This Via:</span>
 		<span><a target="_blank" href="https://twitter.com/intent/tweet?text=<?php the_title(); ?>&url=<?php the_permalink() ?>&via=newdistrictbc">Twitter</a></span>
 		<span><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink() ?>">Facebook</a></span>
 	</ul>
