bfb5940c5ba0caa2c1f978b231d9f573ce19efb8
deadtype <yagnyuk@me.com>
2014-08-12 15:13:09 -0700

    Small update

--- wp-content/themes/bones_clean/farmers-markets.php
+++ wp-content/themes/bones_clean/farmers-markets.php
@@ -9,7 +9,7 @@ Template Name: Farmers Markets Template
  	<?php 	get_template_part('/general_partials/site_header_logo'); ?>
 	
 	<section class="entry-content farmers_markets_content clearfix">
-			<div class='article_content_wrap'>
+			<div class=''>
 				<?php the_content(); ?>
 			</div>
 	</section>
